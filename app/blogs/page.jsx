ad7dd08db23672938bfc39d206997706858ae179
balumaqsud <ollobergan008@mail.ru>
2024-10-11 23:31:51 +0900

    first commit

---
+++ app/blogs/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Blogs = () => {
+  return (
+    <main>
+      <h3>Blogs</h3>
+    </main>
+  );
+};
+
+export default Blogs;
---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>dashboard</h1>
+    </main>
+  );
+}
